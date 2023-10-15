83aef4b5f37355a47612a795da6cb22749928ae5
KsenjA3 <xlyna@yandex.ru>
2023-10-15 19:55:46 +0300

    commit testing2

--- src/InOut/testConnectGit.java
+++ src/InOut/testConnectGit.java
@@ -1,5 +1,9 @@
 package InOut;
 
 public class testConnectGit {
-     System.out.println ("Прочитано: ");
+    public static void main(String[] args) {
+        System.out.println("Прочитано: ");
+        System.out.println("еще один comit ");
+        System.out.println("еще один comit2 ");
+    }
 }
