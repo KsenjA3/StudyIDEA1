8796fb77b2bb6656392ae784287b22f9dbd9f11b
KsenjA3 <xlyna@yandex.ru>
2023-10-16 23:24:35 +0300

    BranchNew comit

--- src/InOut/testConnectGit2.java
+++ src/InOut/testConnectGit2.java
@@ -4,6 +4,6 @@ public class testConnectGit2 {
     public static void main(String[] args) {
         System.out.println("Прочитано: ");
         System.out.println("еще один comit ");
-
+        System.out.println("BranchNew comit ");
     }
 }
