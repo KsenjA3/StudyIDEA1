28b587c92d50bb780ee551028b70794741123953
KsenjA3 <xlyna@yandex.ru>
2023-10-17 13:39:35 +0300

    checkout1

--- src/InOut/testConnectGit2.java
+++ src/InOut/testConnectGit2.java
@@ -7,5 +7,6 @@ public class testConnectGit2 {
         System.out.println("BranchNew comit ");
         System.out.println("BranchNew comit  где?");
         System.out.println("BranchNew12 comit  ");
+        System.out.println("BranchNew12 checkout reset1");
     }
 }
