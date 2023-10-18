f9a76a44296f3b8e6b6d4d8222e7d66b20338e49
KsenjA3 <xlyna@yandex.ru>
2023-10-18 11:40:47 +0300

    testMbN1

--- src/InOut/testConnectGit.java
+++ src/InOut/testConnectGit.java
@@ -4,7 +4,7 @@ public class testConnectGit {
     public static void main(String[] args) {
         System.out.println("Прочитано: ");
         System.out.println("еще один comit ");
-        System.out.println("еще один comit2 ");
+        System.out.println("comit второй ветки для merge");
         System.out.println("еще один comit3 ");
     }
 }
